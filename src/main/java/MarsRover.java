6a9c982c9c4cbbd23fa1086fc436319a976bd312
qlyan(benben) <qlyan@thoughtworks.com>
2018-04-16 00:17:11 +0800

    [yanqilong] abstract method

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -24,6 +24,38 @@ public class MarsRover {
     }
 
     public void exec(String commands) throws Exception {
+        checkCommands(commands);
+
+        for (String command : commands.split("")) {
+            if (command.equals("L")) {
+                turnLeft();
+            } else if (command.equals("R")) {
+                turnRight();
+            } else {
+                moveForward();
+            }
+        }
+    }
+
+    private void turnLeft() {
+        final int index = DIRECTIONS.indexOf(this.direction);
+        this.direction = DIRECTIONS.get(index == 0 ? DIRECTIONS.size() - 1 : index - 1);
+    }
+
+    private void turnRight() {
+        final int index = DIRECTIONS.indexOf(this.direction);
+        this.direction = DIRECTIONS.get(index == DIRECTIONS.size() - 1 ? 0 : index + 1);
+    }
+
+    private void moveForward() {
+        if (this.direction.equals("N") || this.direction.equals("S")) {
+            this.positionY = this.positionY + 1;
+        } else {
+            this.positionX = this.positionX + 1;
+        }
+    }
+
+    private void checkCommands(String commands) throws Exception {
         long count = Arrays.stream(commands.split(""))
                 .distinct()
                 .filter(command -> !command.equals("M") && !command.equals("L") && !command.equals("R"))
@@ -31,23 +63,6 @@ public class MarsRover {
 
         if (count > 0) {
             throw new Exception("Invalid command sequence");
-        }
-
-        for (String command : commands.split("")) {
-            if (command.equals("L")) {
-                final int index = DIRECTIONS.indexOf(this.direction);
-                this.direction = DIRECTIONS.get(index == 0 ? DIRECTIONS.size() - 1 : index - 1);
-            } else if (command.equals("R")) {
-                final int index = DIRECTIONS.indexOf(this.direction);
-                this.direction = DIRECTIONS.get(index == DIRECTIONS.size() - 1 ? 0 : index + 1);
-            } else {
-                if (this.direction.equals("N") || this.direction.equals("S")) {
-                    this.positionY = this.positionY + 1;
-                } else {
-                    this.positionX = this.positionX + 1;
-                }
-
-            }
         }
     }
 
