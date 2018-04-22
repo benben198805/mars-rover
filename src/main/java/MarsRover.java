20e4bde35441b62ffb66e843f90e2f6f0944c869
qlyan(benben) <qlyan@thoughtworks.com>
2018-04-22 10:23:17 +0800

    [yanqilong] MarsRover can receive "LeftCommand"

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,3 +1,25 @@
 public class MarsRover {
 
+    private int x;
+    private int y;
+    private String direction;
+
+    public MarsRover(int x, int y, String direction) {
+
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public void execute(String command) {
+        if (command.equals("R")) {
+            this.direction = "E";
+        } else {
+            this.direction = "S";
+        }
+    }
+
+    public String getLocationAndDirection() {
+        return String.format("%s %s %s", x, y, direction);
+    }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -6,7 +6,20 @@ import static org.junit.Assert.assertThat;
 public class MarsRoverTest {
 
     @Test
-    public void hello_world() throws Exception {
-        assertThat(1 + 1, is(2));
+    public void should_return_00E_when_receive_a_right_command() throws Exception {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        marsRover.execute("R");
+
+        assertThat(marsRover.getLocationAndDirection(), is("0 0 E"));
+    }
+
+    @Test
+    public void should_return_00S_when_receive_a_left_command() {
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+
+        marsRover.execute("L");
+
+        assertThat(marsRover.getLocationAndDirection(), is("0 0 S"));
     }
 }
